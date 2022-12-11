4734b8ee91f42df350b8a7850bb1d4abc78f6ff5
Todd <ihatespam@gmail.com>
2022-12-11 13:40:51 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -658,7 +658,11 @@ const YouTube = frontmatter.youtuber
 <section id="resume" name="resume" style={{ display:'', height:'', overflow:'', fontSize:'clamp(1rem, 1.4vw, 3.2rem)'}}>
 
 <article>
-  
+<div style={{color:'', fontSize:'160%', padding:'0 0 2rem 4rem', textAlign:'center'}}>{companyname}<br />
+{/* {frontmatter.address.addressText}
+<br />
+{frontmatter.address2.addressText2} */}
+</div>
 
   <div id="" className="flexbutt" style={{display:'flex', gap:'40px', justifyContent:'flex-start', alignItems:'baseline', padding:'0 4%'}}>
 
@@ -776,7 +780,7 @@ const YouTube = frontmatter.youtuber
   
 
 
- <div order="1" id="installedbar" className="toolbar hasapp" style={{display:'none', flexDirection:'', gap:'', width:'', borderTop:'1px dotted #666', justifyContent:'', background:'rgba(0, 0, 0, 0.2)', borderRadius:'', padding:'1rem 0', }}>
+ <div order="1" id="installedbar" className="toolbar hasapp print" style={{display:'none', flexDirection:'', gap:'', width:'', borderTop:'1px dotted #666', justifyContent:'', background:'rgba(0, 0, 0, 0.2)', borderRadius:'', padding:'1rem 0', }}>
 
  <div order="2" style={{display:'flex', justifyContent:'center', border:'0px solid red', width:'', margin:'0 auto'}}>
  <Link state={{modal: true}} to="/cover/" className="button print" style={{color:'#fff', fontSize:'clamp(1.2rem, 1.5vw, 3.4rem)', width:'200px', justifyContent:'center',fontWeight:'bold', }}>Cover Letter</Link>
@@ -800,7 +804,7 @@ const YouTube = frontmatter.youtuber
  </div>
 
 
- <div className="toolbar noapp" style={{display:'flex', flexDirection:'', gap:'', width:'', borderTop:'1px dotted #666', justifyContent:'', background:'rgba(0, 0, 0, 0.2)', borderRadius:'', padding:'1rem 0', }}>
+ <div className="toolbar noapp print" style={{display:'flex', flexDirection:'', gap:'', width:'', borderTop:'1px dotted #666', justifyContent:'', background:'rgba(0, 0, 0, 0.2)', borderRadius:'', padding:'1rem 0', }}>
 
 
 <div style={{display:'flex', justifyContent:'', border:'0px solid red', width:'', margin:'0 auto', padding:'4px 0 0 0', minHeight:'30px', maxHeight:'32px'}}><span style={{fontWeight:'bold', fontSize:'1.3rem'}}>Print:</span> &nbsp;<kbd>⌘</kbd> + <kbd>p</kbd> OR <kbd>Ctrl</kbd> + <kbd>p</kbd></div>
